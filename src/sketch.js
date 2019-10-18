b0663856eb7c5edb606f7cc94863ba4673b419ea
-Bonfire- <40984528+BonfireScratch@users.noreply.github.com>
2019-10-18 20:11:00 +0300

    Update sketch.js

--- src/sketch.js
+++ src/sketch.js
@@ -1,24 +1,52 @@
+let WIDTH = window.innerWidth * 0.6;
+let HEIGHT = window.innerHeight * 0.75;
 let noiseScale = 0.01;
 let smoothness = 80;
 let offsetX = 0;
-let speed = 10;
+let offsetY = 0;
+let maxY = HEIGHT * 0.8;
+let blockSize = 2;
+let speedX = 1;
+let speedY = 5;
+let velX = 0;
 
 function setup() {
-    createCanvas(480, 360);
+    createCanvas(WIDTH, HEIGHT);
 }
 
 function draw() {
-	background(0);
+	background(140, 190, 214);
 	
 	if (keyIsDown(LEFT_ARROW)) {
-		offsetX -= speed;
+		velX = (velX - speedX) * 0.9;
 	} else if (keyIsDown(RIGHT_ARROW)) {
-		offsetX += speed;
+		velX = (velX + speedX) * 0.9;
+	} else {
+		velX *= 0.9;
 	}
 	
-	for (let x = 0; x <= width; x++) {
-		let noiseVal = noise((offsetX + x) * noiseScale, noiseScale);
-		stroke(500);
-		line(x, noiseVal * smoothness, x, height);
+	offsetX += velX;
+	
+	if (keyIsDown(UP_ARROW)) {
+		offsetY += speedY;
+	} else if (keyIsDown(DOWN_ARROW)) {
+		offsetY -= speedY;
 	}
+	
+	for (let x = 0; x <= WIDTH; x += blockSize) {
+		let noiseVal = noise((offsetX + x) * noiseScale, noiseScale);
+		let y = noiseVal * smoothness + height + offsetY - maxY;
+		let finalY = round(y / blockSize) * blockSize - 10;
+		noStroke();
+		
+		fill(155, 118, 83);
+		rect(x, HEIGHT, blockSize, finalY - HEIGHT);
+		fill(126, 200, 80);
+		rect(x, finalY, blockSize, 10);
+	}
+}
+
+window.onresize = () => {
+	WIDTH = window.innerWidth * 0.6;
+	HEIGHT = window.innerHeight * 0.75;
 }
